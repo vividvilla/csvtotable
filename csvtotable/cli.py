a40cb0c2d454fc699be8de47f07fef0e5186dbde
Vivek R <vividvilla@gmail.com>
2017-07-09 14:19:51 +0530

    Option to set overwrite output file

--- csvtotable/cli.py
+++ csvtotable/cli.py
@@ -14,13 +14,15 @@ from csvtotable import convert
 @click.option("-dl", "--display-length", type=int, default=-1,
               help=("Number of rows to show by default. "
                     "Defaults to -1 (show all rows)"))
+@click.option("-o", "--overwrite", type=bool, default=False, is_flag=True,
+              help="Overwrite the output file if exisits.")
 def cli(input_file, output_file, caption, delimiter, quotechar,
-        display_length):
+        display_length, overwrite):
     """
     CSVtoTable commandline utility.
     """
     # Prompt for file overwrite if outfile already exists
-    if os.path.exists(output_file):
+    if not overwrite and os.path.exists(output_file):
         fmt = "File ({}) already exists. Do you want to overwrite? (y/n): "
         message = fmt.format(output_file)
         click.secho(message, nl=False, fg="red")

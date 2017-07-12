b2c67e3733dacaaf2150913b8f3809c7f25814cf
Vivek R <vividvilla@gmail.com>
2017-07-12 23:37:41 +0530

    Add function for file overwrite prompt

--- csvtotable/cli.py
+++ csvtotable/cli.py
@@ -4,6 +4,26 @@ import click
 from csvtotable import convert
 
 
+# Prompt for file overwrite
+def prompt_overwrite(file_name):
+    # Skip if file doesn't exist
+    if not os.path.exists(file_name):
+        return
+
+    # Prompt for file overwrite if outfile already exists
+    fmt = "File ({}) already exists. Do you want to overwrite? (y/n): "
+    message = fmt.format(file_name)
+
+    click.secho(message, nl=False, fg="red")
+    choice = click.getchar()
+    click.echo()
+
+    if choice not in ("y", "Y"):
+        return False
+
+    return True
+
+
 @click.command()
 @click.argument("input_file", type=click.Path(exists=True))
 @click.argument("output_file", type=click.Path())
@@ -35,6 +55,8 @@ def cli(input_file, output_file, caption, delimiter, quotechar,
     convert.convert(input_file, output_file, caption=caption,
                     delimiter=delimiter, quotechar=quotechar,
                     display_length=display_length)
+        if not overwrite and not prompt_overwrite(output_file):
+            raise click.Abort()
 
     click.secho("File converted successfully: {}".format(
         output_file), fg="green")

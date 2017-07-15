1688b5bf19bd97dcc99e0547425534ce108aaa49
chfw <wangc_2011@hotmail.com>
2017-07-15 16:26:15 +0100

    remove file compare

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,7 +2,6 @@ import os
 from nose.tools import eq_
 from excel2table.cli import cli
 from click.testing import CliRunner
-from filecmp import cmp
 
 
 def test_cli():
@@ -10,6 +9,4 @@ def test_cli():
 
     result = runner.invoke(cli, ['sample/goog.ods', 'goog.html'])
     eq_(result.exit_code, 0)
-    status = cmp('goog.html', 'sample/goog.html')
     os.unlink('goog.html')
-    assert status is True
